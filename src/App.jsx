a546ad3cda30f45956781b110209535b9a41e051
Martik <martikdavtyanait@gmail.com>
2024-06-18 11:57:07 +0400

    Import dog breeds

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dogBreeds, setDogBreeds] = useState([])
+  const [randomBreeds, setRandomBreeds] = useState([])
 
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then((res) => res.json())
+      .then((data) => {
+        const breeds = Object.keys(data.message)
+        setDogBreeds(breeds)
+      })
+  }, [])
+  console.log(dogBreeds);
   return (
     <>
-      
+    
     </>
   )
 }
