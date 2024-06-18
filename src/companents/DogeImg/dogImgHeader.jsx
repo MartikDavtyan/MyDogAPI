0a83d9517c137d08111e6a082b2b87f22bbaed30
Martik <martikdavtyanait@gmail.com>
2024-06-18 13:10:30 +0400

    Finish all dogeIMGHEADR

--- src/companents/DogeImg/dogImgHeader.jsx
+++ src/companents/DogeImg/dogImgHeader.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import '../../css/DogImg/dogImgHeader.css'
 
 export default function dogImgHeader() {
   return (
-    <div>dogImgHeader</div>
+    <div className="header">
+      <h1>Dog IMG</h1>
+    </div>
   )
 }
