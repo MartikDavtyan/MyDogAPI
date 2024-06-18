f0db5be2f9ca9bf5f6ffa9546b6819b46d25f0f3
Martik <martikdavtyanait@gmail.com>
2024-06-18 12:53:54 +0400

    Add modal code and her css

--- src/companents/DogeName/index.jsx
+++ src/companents/DogeName/index.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import DogeName from './dogeName.jsx'
-import '../../css/DogeName/dogNameMain.css'
+import React, { useState } from 'react';
+import DogeName from './dogeName.jsx';
+import '../../css/DogeName/dogNameMain.css';
 
-export default function dogeName({randomBreeds}) {
+export default function dogeName({ randomBreeds }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedBreed, setSelectedBreed] = useState('');
+
+  const handleClick = (breed) => {
+    setSelectedBreed(breed);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <div className="dogBreeds">
-        {randomBreeds.map((el)=>{
-          return (<>
-            <div className="dogBreed">
-              <DogeName breed={el}/>
-            </div>
-          </>)
-        })}
+        {randomBreeds.map((el, index) => (
+          <div key={index} className="dogBreed" onClick={() => handleClick(el)}>
+            <DogeName breed={el} />
+          </div>
+        ))}
       </div>
+
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleClose}>&times;</span>
+            <h2>{selectedBreed}</h2>
+            <p>Modal content related to {selectedBreed} can go here...</p>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 }
