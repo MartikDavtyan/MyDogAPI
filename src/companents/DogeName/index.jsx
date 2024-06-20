d497972c3f6aec4df1c2646cd71fb7c9424cb5af
MartikDavtyan <martihdavtyanait@gmail.com>
2024-06-20 18:11:14 +0400

    Deleete modal

--- src/companents/DogeName/index.jsx
+++ src/companents/DogeName/index.jsx
@@ -7,24 +7,6 @@ export default function DogeNameComponent({ randomBreeds }) {
   const [selectedBreed, setSelectedBreed] = useState('');
   const [dogIMG, setDogIMG] = useState([]);
 
-  useEffect(() => {
-    if (isOpen && selectedBreed) {
-      fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/3`)
-        .then((res) => res.json())
-        .then((data) => {
-          setDogIMG(data.message);
-        })
-    }
-  }, [isOpen, selectedBreed]);
-
-  const handleClick = (breed) => {
-    setSelectedBreed(breed);
-    setIsOpen(true);
-  };
-
-  const handleClose = () => {
-    setIsOpen(false);
-  };
 
   return (
     <>
@@ -35,20 +17,6 @@ export default function DogeNameComponent({ randomBreeds }) {
           </div>
         ))}
       </div>
-
-      {isOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={handleClose}>&times;</span>
-            <h2>{selectedBreed}</h2>
-            <div className="dogImages">
-              {dogIMG.map((el, index) => (
-                <img key={index} src={el} alt={`Dog ${index + 1}`} />
-              ))}
-            </div>
-          </div>
-        </div>
-      )}
     </>
   );
 }
